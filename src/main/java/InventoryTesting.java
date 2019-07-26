1d032fe47b524fdd53ac97033a3c220f7ab2ff1c
LoneWulf77 <trenton.trawick@gmail.com>
2019-07-25 20:50:49 -0400

    Able to change amounts or add, view, or remove rows in the database table (multiple or single, both via batch operation)

--- src/main/java/InventoryTesting.java
+++ src/main/java/InventoryTesting.java
@@ -1,6 +1,5 @@
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -27,9 +26,28 @@ public class InventoryTesting {
                     addDataToDB(inputArray=addInput());
                 }
 
-                else if(task.equals("remove")||task.equals("remove amount")){
-                    log("\n------ Remove amount from DB ------");
-                    removeAmountFromDB(inputArray=removeInput());
+                else if(task.equals("change amount")||task.equals("change")||task.equals("amounnt")){
+                    System.out.println("Do you want to add or remove items?");
+                    task = userInput();
+
+                    while (true) {
+                        if(task.equals("remove")){
+                            log("\n------ Remove amount from DB ------");
+                            removeAmountFromDB(inputArray= amountInput());
+                            break;
+                        }
+                        else if (task.equals("add")){
+                            log("\n------ Adding amounts to DB ------");
+                            addAmountInDB(inputArray = amountInput());
+                            break;
+                        }
+                        else if(task.equals("menu")){
+                            break;
+                        }
+                        else{
+                            System.out.println("Please enter a valid option or enter menu to return to the main menu.");
+                        }
+                    }
                 }
 
                 else if(task.equals("delete")||task.equals("delete upc")){
@@ -51,11 +69,6 @@ public class InventoryTesting {
                     System.out.println("Please enter a valid action.");
                 }
             }
-
-
-
-
-
 
             myPreparedStat.close();
             myConnect.close();
@@ -153,27 +166,6 @@ public class InventoryTesting {
         }
     }
 
-    private static void removeAmountFromDB(ArrayList<String> inputArray) {
-        try{
-            String getQueryStatement = "DELETE FROM invlist WHERE UPC=(?)";
-            myPreparedStat = myConnect.prepareStatement(getQueryStatement);
-
-            for (int i=0; i+1<inputArray.size(); i++) {
-                myPreparedStat.setInt(1, Integer.parseInt(inputArray.get(i)));
-
-                //add to batch
-                myPreparedStat.addBatch();
-                log(inputArray.get(i) + " added to batch successfully.");
-            }
-
-            int[] inserted = myPreparedStat.executeBatch();
-            log("Batch executed. "+ inserted.length + " records removed from database.");
-
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
-    }
-
     private static void removeUPCFromDB(ArrayList<String> inputArray) {
         try{
             String getQueryStatement = "DELETE FROM invlist WHERE UPC=(?)";
@@ -195,16 +187,82 @@ public class InventoryTesting {
         }
     }
 
-    //log utility
+    private static void removeAmountFromDB(ArrayList<String> inputArray) {
+        subtractAmountInDB(inputArray);
+
+        try{
+            myPreparedStat = myConnect.prepareStatement("SELECT COUNT(amount) AS total FROM invlist WHERE amount<=0");
+
+            ResultSet rs = myPreparedStat.executeQuery();
+
+            int zeroedItems = rs.getInt("total");
+            log(zeroedItems + " records removed.");
+
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+
+        try{
+            myPreparedStat = myConnect.prepareStatement("DELETE FROM invlist WHERE amount<=0");
+            myPreparedStat.executeQuery();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void subtractAmountInDB(ArrayList<String> inputArray){
+        try{
+            String getQueryStatement = "UPDATE invlist SET amount=amount-? WHERE upc=?";
+            myPreparedStat = myConnect.prepareStatement(getQueryStatement);
+
+            for (int i=0; i+1<inputArray.size(); i++) {
+                myPreparedStat.setInt(1, Integer.parseInt(inputArray.get(i+1)));
+                myPreparedStat.setInt(2, Integer.parseInt(inputArray.get(i)));
+
+                //add to batch
+                myPreparedStat.addBatch();
+                log(inputArray.get(i) + " added to batch successfully.");
+            }
+
+            int[] inserted = myPreparedStat.executeBatch();
+            log("Batch executed. "+ inserted.length + " records changed in database.");
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void addAmountInDB(ArrayList<String> inputArray){
+        try{
+            String getQueryStatement = "UPDATE invlist SET amount=amount+? WHERE upc=?";
+            myPreparedStat = myConnect.prepareStatement(getQueryStatement);
+
+            for (int i=0; i+1<inputArray.size(); i++) {
+                myPreparedStat.setInt(1, Integer.parseInt(inputArray.get(i+1)));
+                myPreparedStat.setInt(2, Integer.parseInt(inputArray.get(i)));
+
+                //add to batch
+                myPreparedStat.addBatch();
+                log(inputArray.get(i) + " added to batch successfully.");
+            }
+
+            int[] inserted = myPreparedStat.executeBatch();
+            log("Batch executed. "+ inserted.length + " records changed in database.");
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
     private static void log(String string) {
         System.out.println(string);
     }
 
     private static String userInput(){
         Scanner s = new Scanner(System.in);
-        System.out.println("What would you like to do? Add/Remove Amount/Delete UPC/View/Exit");
+        System.out.println("What would you like to do? Add/Change Amount/Delete UPC/View/Exit");
         return s.nextLine().toLowerCase();
-    }
+    } //main menu options
 
     private static ArrayList<String> addInput(){
         Scanner s = new Scanner(System.in);
@@ -228,7 +286,7 @@ public class InventoryTesting {
         return inputArray;
     }
 
-    private static ArrayList<String> removeInput(){
+    private static ArrayList<String> amountInput(){
         Scanner s = new Scanner(System.in);
 
         while (true){
@@ -239,7 +297,7 @@ public class InventoryTesting {
             inputArray.add(String.valueOf(s.nextInt()));
             s.nextLine();
 
-            System.out.println("Do you have more items to remove? y/n");
+            System.out.println("Do you have more items to change? y/n");
             if("n".equals(s.nextLine())){
                 break;
             }
